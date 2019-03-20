b4c6e74b4d8676097c3085346619ce87d34949aa
Martin Melo <nitram-210397@hotmail.com>
2019-03-20 13:38:55 -0600

    Added axios in backend

--- backend_kaya/routes/index.js
+++ backend_kaya/routes/index.js
@@ -3,7 +3,12 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  //res.render('index');
+  res.json({"hola":"Hello World"})
 });
+
+router.get('/:pepe', (req, res) => {
+  res.json({"nombre": req.params.pepe})
+})
 
 module.exports = router;
