be502d36e855659445f31f4b0f0b5c764c9dc2e3
Martin Melo <nitram-210397@hotmail.com>
2019-03-20 13:55:33 -0600

    Consuming mapbox api to get the info of a city dynamic

---
+++ backend_kaya/consts.js
@@ -0,0 +1,3 @@
+module.exports = {
+  getLatLongcity: "https://api.mapbox.com/geocoding/v5/mapbox.places/.json?access_token=pk.eyJ1IjoiamdhdmlyMjMiLCJhIjoiY2pwMzMwanYwMDJkeTNwcDduODR5bXRlayJ9.p_HLVm6sK-X0d5-JIpSdxA"
+}
--- backend_kaya/routes/index.js
+++ backend_kaya/routes/index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router  = express.Router();
-
+const axios = require('axios')
+const consts = require('../consts')
 /* GET home page */
 router.get('/', (req, res, next) => {
   //res.render('index');
   res.json({"hola":"Hello World"})
 });
 
-router.get('/:pepe', (req, res) => {
-  res.json({"nombre": req.params.pepe})
+router.get('/:city', (req, res) => {
+  let city = req.params.city
+  axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoiamdhdmlyMjMiLCJhIjoiY2pwMzMwanYwMDJkeTNwcDduODR5bXRlayJ9.p_HLVm6sK-X0d5-JIpSdxA`)
+    .then((response)=>{
+      console.log(response.data)
+      res.json(response.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+      res.json(JSON.stringify(err))
+    })
+
+    //res.json({"hola":"hola"})
+    
 })
 
 module.exports = router;
